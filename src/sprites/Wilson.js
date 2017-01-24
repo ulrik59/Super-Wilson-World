6fe4a07f4bf054fc507791ca2cfec59794ac12a8
Martin Lenglet <martin.lenglet@francetv.fr>
2017-01-24 14:08:15 +0100

    refactor(wilson): Add all wilson properties to his own class

--- src/sprites/Wilson.js
+++ src/sprites/Wilson.js
@@ -4,19 +4,74 @@ class Wilson extends Phaser.Sprite {
   constructor({ game, x, y, asset }) {
     super(game, x, y, asset);
 
-    game.physics.enable(this, Phaser.Physics.ARCADE);
+    this.game = game;
+    this.game.physics.enable(this, Phaser.Physics.ARCADE);
+
     this.body.bounce.y = 0.2;
     this.body.collideWorldBounds = true;
     this.body.setSize(20, 32, 5, 16);
 
-    // this.animations.add('left', [0, 1, 2, 3], 10, true);
-    // this.animations.add('turn', [4], 20, true);
-    // this.animations.add('right', [5, 6, 7, 8], 10, true);
+    this.animations.add('left', [0, 1, 2, 3], 10, true);
+    this.animations.add('turn', [4], 20, true);
+    this.animations.add('right', [5, 6, 7, 8], 10, true);
+
+    this.facing = 'left';
+    this.jumpTimer = 0;
   }
 
-  // update() {
-  //   this.body.velocity.x = 0;
-  // }
+  update() {
+    this.game.physics.arcade.collide(this, this.game.layer);
+    this.body.velocity.x = 0;
+
+    if (this.game.cursors.left.isDown) {
+      this.moveLeft();
+    } else if (this.game.cursors.right.isDown) {
+      this.moveRight();
+    } else if (this.facing !== 'idle') {
+      this.stopMoving();
+    }
+
+    if (this.game.spacebar.isDown) {
+      this.jump();
+    }
+  }
+
+  moveRight() {
+    this.body.velocity.x = 150;
+
+    if (this.facing !== 'right') {
+      this.animations.play('right');
+      this.facing = 'right';
+    }
+  }
+
+  moveLeft() {
+    this.body.velocity.x = -150;
+
+    if (this.facing !== 'left') {
+      this.animations.play('left');
+      this.facing = 'left';
+    }
+  }
+
+  stopMoving() {
+    this.animations.stop();
+
+    if (this.facing === 'left') {
+      this.frame = 0;
+    } else {
+      this.frame = 5;
+    }
+
+    this.facing = 'idle';
+  }
+
+  jump() {
+    if (this.body.onFloor() && this.game.time.now > this.jumpTimer) {
+      this.body.velocity.y = -250;
+      this.jumpTimer = this.game.time.now + 750;
+    }
+  }
 }
 
 export default Wilson;
--- src/states/Level1.js
+++ src/states/Level1.js
@@ -1,20 +1,8 @@
 /* globals __DEV__ */
 import Phaser from 'phaser-ce';
-// import Wilson from '../sprites/Wilson';
+import Wilson from '../sprites/Wilson';
 
 class Level1 extends Phaser.State {
-  init() {
-    this.map = null;
-    this.tileset = null;
-    this.layer = null;
-    this.wilson = null;
-    this.facing = 'left';
-    this.jumpTimer = 0;
-    this.cursors = null;
-    this.jumpButton = null;
-    this.bg = null;
-  }
-
   create() {
     this.stage.backgroundColor = '#000';
 
@@ -25,63 +13,19 @@ class Level1 extends Phaser.State {
     this.map.addTilesetImage('tiles-1');
     this.map.setCollisionByExclusion([13, 14, 15, 16, 46, 47, 48, 49, 50, 51]);
 
-    this.layer = this.map.createLayer('Tile Layer 1');
-    this.layer.resizeWorld();
+    this.game.layer = this.map.createLayer('Tile Layer 1');
+    this.game.layer.resizeWorld();
 
     this.physics.arcade.gravity.y = 250;
 
-    this.wilson = this.add.sprite(32, 32, 'dude');
-    this.physics.enable(this.wilson, Phaser.Physics.ARCADE);
-
-    this.wilson.body.bounce.y = 0.2;
-    this.wilson.body.collideWorldBounds = true;
-    this.wilson.body.setSize(20, 32, 5, 16);
-
-    this.wilson.animations.add('left', [0, 1, 2, 3], 10, true);
-    this.wilson.animations.add('turn', [4], 20, true);
-    this.wilson.animations.add('right', [5, 6, 7, 8], 10, true);
+    this.wilson = new Wilson({ game: this.game, x: 32, y: 32, asset: 'dude' });
+    this.add.existing(this.wilson);
 
     this.camera.follow(this.wilson);
   }
 
-  update() {
-    const cursors = this.game.cursors;
-    const wilson = this.wilson;
-
-    this.physics.arcade.collide(this.wilson, this.layer);
-    wilson.body.velocity.x = 0;
-
-    if (cursors.left.isDown) {
-      wilson.body.velocity.x = -150;
-
-      if (this.facing !== 'left') {
-        wilson.animations.play('left');
-        this.facing = 'left';
-      }
-    } else if (cursors.right.isDown) {
-      wilson.body.velocity.x = 150;
-
-      if (this.facing !== 'right') {
-        wilson.animations.play('right');
-        this.facing = 'right';
-      }
-    } else if (this.facing !== 'idle') {
-      wilson.animations.stop();
-
-      if (this.facing === 'left') {
-        wilson.frame = 0;
-      } else {
-        wilson.frame = 5;
-      }
-
-      this.facing = 'idle';
-    }
-
-    if (this.game.spacebar.isDown && wilson.body.onFloor() && this.time.now > this.jumpTimer) {
-      this.wilson.body.velocity.y = -250;
-      this.jumpTimer = this.time.now + 750;
-    }
-  }
+  // update() {
+  // }
 
   render() {
     if (__DEV__) {
